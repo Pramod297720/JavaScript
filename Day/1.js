4bba2a535a1aca9570eefdfd336ad396fed7f581
Pramod Adhikari <120573449+Pramod297720@users.noreply.github.com>
2025-04-15 09:49:25 +0000

    day 2

---
+++ Day/1.js
@@ -0,0 +1,5 @@
+// Day 1: 
+
+// // Learn to print HEllo world in the github worplace to print the statement 
+
+console.log("Hello world")
---
+++ Day/2.js
@@ -0,0 +1,20 @@
+// in this section we will learn about thevariable and its type and why it is used
+
+// 1 const 
+const name = "Pramod Adhikari";
+// const is used so that i do not have to chaneg the variable as we can't chaneg the variable of the code
+
+let phoneNumber=123455;
+// we use let to declaret he variable and the variable is change able as when we write 
+// let phoneNumber= 123456;
+// we can change the varibale and use is it elsewhere 
+
+var sex='male'
+// we don't use var in any of the type as its shows error when delcaring scope and what is scope? scope is the declaration mostly used in the fucntion and otehr delcaration 
+// such as in if, for or user defined fucntion 
+
+address = "nepal"
+
+//without delcaration aslo we can use the variabel but its aa very bad practice and don't used nowdayd 
+
+console.log([name,address,sex,phoneNumber])
