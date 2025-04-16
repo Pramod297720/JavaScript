f11cfceb54508cee1ae5639194b7c09548cecda5
Pramod Adhikari <120573449+Pramod297720@users.noreply.github.com>
2025-04-16 17:26:05 +0000

    datatypes

---
+++ Day/part5.js
@@ -0,0 +1,35 @@
+//conversion operation
+
+//number into other datatypes
+let number = 3;
+let changeNumber = String(number);
+let changeNumber1 = Boolean(number);
+
+console.log(changeNumber);
+console.log(typeof(changeNumber));
+console.log(changeNumber1);
+console.log(typeof(changeNumber1));
+
+
+// // String into other datatypes
+let someString = "Hello"
+let changeString = Number(someString);
+let changeString1 = Boolean(someString);
+
+console.log(changeString);
+console.log(typeof(changeString));
+console.log(changeString1);
+console.log(typeof(changeString1));
+
+//boleaninto others datatypes
+
+let isBoolean = true;
+console.log(typeof isBoolean);
+
+let changeBoolean = Number(isBoolean);
+let changeBoolean1 = String(isBoolean);
+
+console.log(changeBoolean);
+console.log(typeof(changeBoolean));
+console.log(changeBoolean1);
+console.log(typeof(changeBoolean1));
---
+++ Day/part6.js
@@ -0,0 +1,10 @@
+//operation
+
+let firstNumber = 3;
+let secondNumber = -firstNumber;
+let thirdNumber = 10 - secondNumber;
+console.log(secondNumber);
+
+console.log(thirdNumber);
+console.log(typeof(firstNumber))
+console.log(secondNumber - firstNumber);
