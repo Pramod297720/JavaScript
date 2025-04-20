e75cb88c0ed4ce9c2310c7275f68d3845bc76fab
Pramod Adhikari <120573449+Pramod297720@users.noreply.github.com>
2025-04-20 19:42:38 +0000

    bsci complete

--- part10.js
+++ part10.js
@@ -1,2 +1,13 @@
 //Date and times
 
+let mydate = new Date();
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+let myCreatedDate = new Date(2023, 0 , 23);
+console.log(myCreatedDate.toString());
+
+let dateNow = Date.now();
+console.log(dateNow);
+
